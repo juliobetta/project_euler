473c81ca27a9cab0fc4d94c1da68bccb3211705a
Julio Betta <juliobetta@gmail.com>
2013-11-06 01:37:04 -0200

    change a little bit =)

--- 01_multiples_of_3_and_5.rb
+++ 01_multiples_of_3_and_5.rb
@@ -1,6 +1,5 @@
 require('rspec')
 
-
 # MULTIPLES OF 3 and 5
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 # The sum of these multiples is 23.
@@ -9,15 +8,15 @@ require('rspec')
 
 # IMPLEMENTATION
 # ===================================================================
-def calculate(n)
+def calculate_below(n)
   (1...n).map{|n| n if n % 3 == 0 or n % 5 == 0}.compact!.reduce(:+)
 end
 
 
 # SPEC
 # ===================================================================
-describe 'Find the sum of all the multiples of 3 or 5 below a given number.' do
-  it 'should find 23 until 10' do
-    calculate(10).should == 23
+describe 'Find the sum of all the multiples of 3 or 5 below a given number' do
+  it 'should find 23 from 1 to 10' do
+    calculate_below(10).should == 23
   end
 end
